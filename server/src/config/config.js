90e0948d70d80c6531a53b9879c606954bc1d9a5
Kristin Mayo <krissimayo@gmail.com>
2019-03-14 00:03:59 -0400

    implement hashing algorithm for passwords

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -10,5 +10,8 @@ module.exports = {
             storage: './fitness-app.sqlite'
 
         }
+    },
+    authentication: {
+        jwtSecret: process.env.JWT_SECRET || 'secret'
     }
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -2,7 +2,10 @@ const AuthenticationController = require('./controllers/AuthenticationController
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
 
 module.exports = (app) => {
-  app.post('/register',    
+  app.post('/register',
     AuthenticationControllerPolicy.register,
-    AuthenticationController.register)    
+    AuthenticationController.register)
+
+  app.post('/login',
+    AuthenticationController.login)
 }
