c9c16640f7f727acfb04a54655bb269cdc4064b4
Kristin Mayo <krissimayo@gmail.com>
2019-03-13 22:12:59 -0400

    add fixed header

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,24 +1,37 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input
-        type="email"
-        name="email"
-        v-model="email"
-        placeholder="email" />
-    <br>
-    <input
-        type="password"
-        name="password"
-        v-model="password"
-        placeholder="password" />
-    <br>
-    <button
-        @click="register">
-        Register
-        </button>
-  </div>
-</template>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-text-field
+            label="Email"
+            v-model="email"
+          ></v-text-field>
+          <br>
+
+          <v-text-field
+            label="Password"
+            v-model="password"
+          ></v-text-field>
+          <br>
+
+          <div class="error" v-html="error" />
+          <br>
+          <v-btn
+            dark
+            class="cyan"
+            @click="register">
+            Register
+          </v-btn>
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
+ </template>
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService'
@@ -45,8 +58,9 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.error {
+  color: red;
+}
 
 </style>
