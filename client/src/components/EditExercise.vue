479048091eadd565ebd7d8010dff2e1864430db8
Kristin Mayo <krissimayo@gmail.com>
2019-05-13 11:54:07 -0400

    begin implementation of autocomplete

--- client/src/components/EditExercise.vue
+++ client/src/components/EditExercise.vue
@@ -3,14 +3,45 @@
     <v-flex xs6 offset-xs3>
       <div class="white">
         <v-toolbar flat dense class="blue darken-2" dark>
-          <v-toolbar-title>New Exercise</v-toolbar-title>
+          <v-toolbar-title>Edit Exercise</v-toolbar-title>
         </v-toolbar>
+
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <v-text-field
+            label="Title"
+            v-model="exercise.title"
+          ></v-text-field>
+          <br>
+
+          <v-text-field
+            label="Target muscle group"
+            v-model="exercise.muscleGroup"
+          ></v-text-field>
+          <br>
+
+          <v-text-field
+            label="Goal type"
+            v-model="exercise.goalGroup"
+          ></v-text-field>
+          <br>
+
+          <v-text-field
+            label="Difficulty level"
+            v-model="exercise.difficulty"
+          ></v-text-field>
+          <br>
+
+          <v-text-field
+            label="Image URL"
+            v-model="exercise.imageURL"
+          ></v-text-field>
+
           <br>
           <v-btn
             dark
             class="blue darken-2"
-            @click="create">
-            Submit
+            @click="save">
+            Save
           </v-btn>
           <v-btn
             dark
@@ -19,6 +50,7 @@
             Cancel
           </v-btn>
         </div>
+      </div>
     </v-flex>
   </v-layout>
 </template>
@@ -41,12 +73,21 @@ export default {
     navigateTo (route) {
       this.$router.push(route)
     },
-    async create () {
+    async save () {
+      console.log('save clicked')
       try {
-        await ExerciseService.post(this.exercise)
+        await ExerciseService.put(this.exercise)
         this.$router.push({
           name: 'profile'
         })
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    async mounted () {
+      try {
+        const exerciseId = this.$store.state.route.params.exerciseId
+        this.exercise = (await ExerciseService.show(exerciseId)).data
       } catch (err) {
         console.log(err)
       }
